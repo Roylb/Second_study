5b42728a1f0eea520f8c79075eb91ed243d3fd91
roy <liulibin_cmb@etc.com>
2017-02-26 17:21:03 +0800

    build gradle.build

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,25 +1,14 @@
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTest {
 
-    public static void main(String[] asgr) {
+    @Test
+    public void test() {
 
-        try {
-            URL url = new URL("http://localhost:8080/test");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            StringBuffer buffer = new StringBuffer();
-            buffer.append("name").append("=").append("zcq").append("&").append("age").append("=").append("18");
-            connection.getOutputStream().write(buffer.toString().getBytes());
-            connection.getOutputStream().flush();
-            connection.getOutputStream().close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
